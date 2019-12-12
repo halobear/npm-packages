f74ff05f775601f9d39892b1cc398611caaf9cfc
luzhongkuan <luzhongk@126.com>
2019-12-12 18:36:40 +0800

    chore: 仓库路径

--- packages/vue-request/CHANGELOG.md
+++ packages/vue-request/CHANGELOG.md
@@ -1,69 +1,41 @@
-## [0.0.9](https://github.com/kuan1/luzhongk/compare/v0.0.8...v0.0.9) (2019-12-02)
+## [0.0.9](https://github.com/halobear/npm-packages/compare/v0.0.8...v0.0.9) (2019-12-02)
 
+## [0.0.8](https://github.com/halobear/npm-packages/compare/v0.0.7...v0.0.8) (2019-12-02)
 
-
-## [0.0.8](https://github.com/kuan1/luzhongk/compare/v0.0.7...v0.0.8) (2019-12-02)
-
-
-
-## [0.0.7](https://github.com/kuan1/luzhongk/compare/v0.0.6...v0.0.7) (2019-12-02)
-
+## [0.0.7](https://github.com/halobear/npm-packages/compare/v0.0.6...v0.0.7) (2019-12-02)
 
 ### Features
 
-* vue-shape ([8b67164](https://github.com/kuan1/luzhongk/commit/8b671649a4efa3a96ae838ead7241748610a1bdd))
+- vue-shape ([8b67164](https://github.com/halobear/npm-packages/commit/8b671649a4efa3a96ae838ead7241748610a1bdd))
 
+## [0.0.6](https://github.com/halobear/npm-packages/compare/v0.0.5...v0.0.6) (2019-11-18)
 
-
-## [0.0.6](https://github.com/kuan1/luzhongk/compare/v0.0.5...v0.0.6) (2019-11-18)
-
-
-
-## [0.0.5](https://github.com/kuan1/luzhongk/compare/v0.0.4...v0.0.5) (2019-11-11)
-
+## [0.0.5](https://github.com/halobear/npm-packages/compare/v0.0.4...v0.0.5) (2019-11-11)
 
 ### Bug Fixes
 
-* **vue-request:** package.json ([99a4556](https://github.com/kuan1/luzhongk/commit/99a45568580fe045c926ce315d62958251afe8a0))
+- **vue-request:** package.json ([99a4556](https://github.com/halobear/npm-packages/commit/99a45568580fe045c926ce315d62958251afe8a0))
 
+## [0.0.3](https://github.com/halobear/npm-packages/compare/v0.0.2...v0.0.3) (2019-11-05)
 
+## [0.0.2](https://github.com/halobear/npm-packages/compare/v0.0.2-alpha.0...v0.0.2) (2019-11-04)
 
-## [0.0.3](https://github.com/kuan1/luzhongk/compare/v0.0.2...v0.0.3) (2019-11-05)
-
-
-
-## [0.0.2](https://github.com/kuan1/luzhongk/compare/v0.0.2-alpha.0...v0.0.2) (2019-11-04)
-
-
-
-## [0.0.2-alpha.0](https://github.com/kuan1/luzhongk/compare/v0.0.1...v0.0.2-alpha.0) (2019-11-04)
-
+## [0.0.2-alpha.0](https://github.com/halobear/npm-packages/compare/v0.0.1...v0.0.2-alpha.0) (2019-11-04)
 
 ### Features
 
-* build ([f621412](https://github.com/kuan1/luzhongk/commit/f6214123111a1056f87843563c4c92809053834c))
+- build ([f621412](https://github.com/halobear/npm-packages/commit/f6214123111a1056f87843563c4c92809053834c))
 
+## [0.0.1](https://github.com/halobear/npm-packages/compare/v0.0.1-alpha.0...v0.0.1) (2019-11-03)
 
-
-## [0.0.1](https://github.com/kuan1/luzhongk/compare/v0.0.1-alpha.0...v0.0.1) (2019-11-03)
-
-
-
-## [0.0.1-alpha.0](https://github.com/kuan1/luzhongk/compare/1.0.1...v0.0.1-alpha.0) (2019-11-03)
-
+## [0.0.1-alpha.0](https://github.com/halobear/npm-packages/compare/1.0.1...v0.0.1-alpha.0) (2019-11-03)
 
 ### Bug Fixes
 
-* 测试 ([61042b7](https://github.com/kuan1/luzhongk/commit/61042b7aa0291e9a8094fc333c0396feeaed3ac2))
-
+- 测试 ([61042b7](https://github.com/halobear/npm-packages/commit/61042b7aa0291e9a8094fc333c0396feeaed3ac2))
 
 ### Features
 
-* **vue-feedback:** 项目初始化 ([d25412f](https://github.com/kuan1/luzhongk/commit/d25412fb0004e586af1cb59f766ed95bf3847411))
-
-
+- **vue-feedback:** 项目初始化 ([d25412f](https://github.com/halobear/npm-packages/commit/d25412fb0004e586af1cb59f766ed95bf3847411))
 
 # 1.0.0 (2019-10-31)
-
-
-
--- scripts/createJSON.js
+++ scripts/createJSON.js
@@ -1,76 +1,76 @@
-const fs = require('fs')
-const path = require('path')
-const chalk = require('chalk')
+const fs = require("fs");
+const path = require("path");
+const chalk = require("chalk");
 
-const { input } = require('./utils/feedback')
+const { input } = require("./utils/feedback");
 
-function getJson(name = '插件名字', description = '插件描述') {
+function getJson(name = "插件名字", description = "插件描述") {
   return {
     name: `@luzhongk/${name}`,
-    version: '0.0.4',
+    version: "0.0.4",
     description,
-    keywords: ['vue'],
+    keywords: ["vue"],
     scripts: {
-      dev: 'yarn serve',
-      serve: 'vue-cli-service serve',
-      build: 'vue-cli-service build',
-      'build:lib': `vue build index.js -t lib -d lib -n ${name}`,
-      lint: 'vue-cli-service lint',
+      dev: "yarn serve",
+      serve: "vue-cli-service serve",
+      build: "vue-cli-service build",
+      "build:lib": `vue build index.js -t lib -d lib -n ${name}`,
+      lint: "vue-cli-service lint",
       version:
         "conventional-changelog -p angular -i CHANGELOG.md -s -r 0 --commit-path '.'"
     },
-    author: 'luzhongkuan <luzhongk@126.com>',
-    homepage: `https://github.com/kuan1/luzhongk/tree/master/packages/${name}`,
-    license: 'ISC',
+    author: "luzhongkuan <luzhongk@126.com>",
+    homepage: `https://github.com/halobear/npm-packages/tree/master/packages/${name}`,
+    license: "ISC",
     main: `lib/${name}.umd.min.js`,
     directories: {
-      lib: 'lib'
+      lib: "lib"
     },
-    files: ['lib', 'src'],
+    files: ["lib", "src"],
     publishConfig: {
-      access: 'public'
+      access: "public"
     },
     repository: {
-      type: 'git',
-      url: 'git+https://github.com/kuan1/luzhongk.git'
+      type: "git",
+      url: "git+https://github.com/halobear/npm-packages.git"
     },
     bugs: {
-      url: 'https://github.com/kuan1/luzhongk/issues'
+      url: "https://github.com/halobear/npm-packages/issues"
     },
     dependencies: {
-      'core-js': '^3.3.2',
-      vue: '^2.6.10'
+      "core-js": "^3.3.2",
+      vue: "^2.6.10"
     },
     devDependencies: {
-      '@vue/cli-plugin-babel': '^3.7.0',
-      '@vue/cli-plugin-eslint': '^3.7.0',
-      '@vue/cli-service': '^3.7.0',
-      'babel-eslint': '^10.0.1',
-      eslint: '^5.16.0',
-      'eslint-plugin-vue': '^5.0.0',
-      less: '^3.10.3',
-      'less-loader': '^5.0.0',
-      'vue-template-compiler': '^2.5.21'
+      "@vue/cli-plugin-babel": "^3.7.0",
+      "@vue/cli-plugin-eslint": "^3.7.0",
+      "@vue/cli-service": "^3.7.0",
+      "babel-eslint": "^10.0.1",
+      eslint: "^5.16.0",
+      "eslint-plugin-vue": "^5.0.0",
+      less: "^3.10.3",
+      "less-loader": "^5.0.0",
+      "vue-template-compiler": "^2.5.21"
     }
-  }
+  };
 }
 
 function isExist(name) {
-  const dir = path.resolve(__dirname, '../packages', name, 'package.json')
-  return fs.existsSync(dir)
+  const dir = path.resolve(__dirname, "../packages", name, "package.json");
+  return fs.existsSync(dir);
 }
 
 async function createJson() {
-  const name = await input('请输入插件名字')
-  if (isExist(name)) return console.error(chalk.red(`${name}插件已经存在了`))
-  const desc = await input('请输入插件描述')
-  const json = getJson(name, desc)
+  const name = await input("请输入插件名字");
+  if (isExist(name)) return console.error(chalk.red(`${name}插件已经存在了`));
+  const desc = await input("请输入插件描述");
+  const json = getJson(name, desc);
   fs.writeFileSync(
     `${__dirname}/package.json`,
     JSON.stringify(json, null, 2),
-    'utf-8'
-  )
-  console.log(chalk.green(`${name}json创建成功`))
+    "utf-8"
+  );
+  console.log(chalk.green(`${name}json创建成功`));
 }
 
-createJson()
+createJson();
