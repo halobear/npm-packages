7b8bf49c9ca6dc40e360023f33df992a522875b8
luzhongkuan <luzhongk@126.com>
2021-03-29 17:38:31 +0800

    chore: vite

---
+++ packages/vue-shape/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <h2>测试vite</h2>
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <button @click="add">点击</button>
+  <span>{{ count }}</span>
+</template>
+
+<script>
+import { ref } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  components: {
+    HelloWorld,
+  },
+  setup() {
+    const count = ref(1);
+    return {
+      count,
+      add: () => (count.value += 1),
+    };
+  },
+};
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ packages/vue-shape/src/components/HelloWorld.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>测试</h2>
+</template>
